52cb3d498c24d922ad38dd543af14a82f7c4970a
Neeraj-Kumar07 <neerajkumar040403@gmail.com>
2026-03-22 20:58:41 +0530

    db changes

--- insurance_project/apps/insurance/urls.py
+++ insurance_project/apps/insurance/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('admin-dashboard', views.admin_dashboard_view, name='admin-dashboard'),
     
     
-    path('adminlogin', LoginView.as_view(template_name='insurance/adminlogin.html'), name='adminlogin'),
+    path('adminlogin', LoginView.as_view(template_name='insurance/adminlogin.html'), name='adminlogin/'),
     path('adminclick', views.adminclick_view, name='adminclick'),
 ]
