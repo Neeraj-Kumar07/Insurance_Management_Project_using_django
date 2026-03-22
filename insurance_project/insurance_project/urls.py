f46bb96a81938d76bd320609e1bf0d00205f57f0
Neeraj-Kumar07 <neerajkumar040403@gmail.com>
2026-03-22 17:35:43 +0530

    Fixed URL routing for home page

--- insurance_project/insurance_project/urls.py
+++ insurance_project/insurance_project/urls.py
@@ -14,11 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+#==========================================================================#
+# from django.contrib import admin
+# from django.urls import path
+
+# urlpatterns = [
+#     path("admin/", admin.site.urls),
+    
+# ]
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include # Add 'include' here
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    
+    path("", include("apps.insurance.urls")), # This connects your home page
+    path("customer/", include("apps.customer.urls")), # Connects customer features
 ]
+#========================================================================================#
