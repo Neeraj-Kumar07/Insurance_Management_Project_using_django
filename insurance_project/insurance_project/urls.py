591bbfdd31f6c65fffc87950484ba1dffedfeb55
Neeraj-Kumar07 <neerajkumar040403@gmail.com>
2026-03-22 19:40:28 +0530

    db changes

--- insurance_project/insurance_project/urls.py
+++ insurance_project/insurance_project/urls.py
@@ -27,7 +27,7 @@ from django.contrib import admin
 from django.urls import path, include # Add 'include' here
 
 urlpatterns = [
-    path("adminlogin/", admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", include("apps.insurance.urls")), # This connects  home page
     path("customer/", include("apps.customer.urls")), # Connects customer features
 ]
