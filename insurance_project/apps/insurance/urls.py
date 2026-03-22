9125a9a937bd9c4af3e82c3989af9ac50368d0a6
Neeraj-Kumar07 <neerajkumar040403@gmail.com>
2026-03-22 19:07:57 +0530

    db changes

--- insurance_project/apps/insurance/urls.py
+++ insurance_project/apps/insurance/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView # Add this import
 from . import views
 
 urlpatterns = [ 
-    path('', views.home_view, name='home'), # This is the missing link!
+    path('', views.home_view, name='home'),
     path('aboutus', views.aboutus_view, name='aboutus'),
     path('contactus', views.contactus_view, name='contactus'),
     path('afterlogin', views.afterlogin_view, name='afterlogin'),
     path('admin-dashboard', views.admin_dashboard_view, name='admin-dashboard'),
     
+    
+    path('adminlogin', LoginView.as_view(template_name='insurance/adminlogin.html'), name='adminlogin'),
+    path('adminclick', views.adminclick_view, name='adminclick'),
 ]
